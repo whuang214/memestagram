fe8a7f1f8d2752dd9267caa7b06824a51a9653fd
whuang214 <whuang2146@gmail.com>
2023-07-24 17:54:21 -0400

    add home page

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -10,6 +10,3 @@ const db = mongoose.connection;
 db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
-
-
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Home from "./pages/HomePage/Home";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
+      <Routh path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
