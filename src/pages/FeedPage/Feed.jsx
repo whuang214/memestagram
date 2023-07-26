f9cf773d9356dbe3924ca8c012ef1f30ac92c66c
whuang214 <whuang2146@gmail.com>
2023-07-26 17:42:16 -0400

    add feed page after login

--- src/pages/FeedPage/Feed.jsx
+++ src/pages/FeedPage/Feed.jsx
@@ -1,6 +1,39 @@
-import { Card } from "antd";
-import "./feed.css"; // Importing the CSS
+import { List, Card, Image } from "antd";
+import { InstagramOutlined } from "@ant-design/icons";
+import "./feed.css";
 
 export default function Feed() {
-  return <h1>Welcome to the Feed Page</h1>;
+  const sampleData = [
+    {
+      title: "Post 1",
+      content: "This is the content for post 1.",
+      imageUrl: "https://via.placeholder.com/500", // placeholder image
+    },
+    {
+      title: "Post 2",
+      content: "This is the content for post 2.",
+      imageUrl: "https://via.placeholder.com/500",
+    },
+  ];
+
+  return (
+    <div className="feed-container">
+      <h1>
+        <InstagramOutlined /> Memestagram
+      </h1>
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={sampleData}
+        renderItem={(item) => (
+          <List.Item>
+            <Card title={item.title}>
+              <Image src={item.imageUrl} alt={item.title} width={500} />
+              <p>{item.content}</p>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 }
